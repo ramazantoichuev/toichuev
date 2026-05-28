997c365359077edc4217b15302c6e3ddd172c02d
Ramazan <ramazantoichuev16@gmail.com>
2026-05-28 13:47:06 +0600

    added postgres db and installed psycopg2

--- source/source/settings.py
+++ source/source/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'source.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_todo_list',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
