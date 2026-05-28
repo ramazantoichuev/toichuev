8cc500df1d0b77454b74b16106e3158c9fc974fd
Ramazan <ramazantoichuev16@gmail.com>
2026-05-28 14:19:36 +0600

    moved the template files to the root directory

--- source/to_do/views.py
+++ source/to_do/views.py
@@ -7,21 +7,21 @@ from to_do.models import Task
 
 def tasks(request):
     tasks = Task.objects.all()
-    return render(request, "index.html", {"tasks": tasks})
+    return render(request, "articles/index.html", {"tasks": tasks})
 
 def task(request):
     id = request.GET.get('id')
     if id:
         try:
             task = Task.objects.get(id=int(id))
-            return render(request, "task_view.html", {"task": task})
+            return render(request, "articles/task_view.html", {"task": task})
         except Task.DoesNotExist:
             return HttpResponseRedirect("/tasks")
     return HttpResponseRedirect("/tasks")
 
 def task_create_view(request):
     if request.method == 'GET':
-        return render(request, 'task_create.html')
+        return render(request, 'articles/task_create.html')
     elif request.method == 'POST':
         Task.objects.create(
             description=request.POST.get("description"),
